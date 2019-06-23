4edb0c1b50b3f354a812d81e18e63a5b8b0d0656
Chamith95 <chamithjay@gmail.com>
2019-06-23 11:49:24 +0530

    first commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +10,21 @@ export class LoginComponent implements OnInit {
 
   username="chamith"
   password=""
+  errorMessage="Invalid Credentials"
+  invalidLogin =false;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  handleLogin(){
+    console.log(this.username);
+    if(this.username ==="chamith" && this.password ==='123'){
+      this.router.navigate(['welcome',this.username])
+      this.invalidLogin =false;
+    }else{
+      this.invalidLogin=true;
+    }
+  }
 }
