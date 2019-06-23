3426462f99a3196eb5cca1f1382e933b342bd499
Chamith95 <chamithjay@gmail.com>
2019-06-23 14:48:30 +0530

    added bootrap

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  constructor(
+    public id:number,
+    public description:string,
+    public done:boolean,
+    public targetDate:Date,
+  ){
+
+  }
+}
+
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +20,13 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos=[
-    {id: 1,description:"Learn to Dance"},
-    {id: 2,description:"Become an Expert at angular"},
-    {id: 3,description:"Visit india"}
+    new Todo(1,"Learn To do",false,new Date()),
+    new Todo(2,"Learn spring",true,new Date()),
+    new Todo(3,"Learn Hibernate",false,new Date()),
+    
+    // {id: 1,description:"Learn to Dance"},
+    // {id: 2,description:"Become an Expert at angular"},
+    // {id: 3,description:"Visit india"}
   ]
 
   // todo={
