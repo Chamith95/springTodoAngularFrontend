da471b7a60e0182d97481c1cf2a56bf9c0db9608
Chamith95 <chamithjay@gmail.com>
2019-06-24 14:56:41 +0530

    update todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from '../services/data/todo-data.service';
 import { Todo } from '../list-todos/list-todos.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -13,7 +13,7 @@ export class TodoComponent implements OnInit {
   id:number
   todo:Todo
 
-  constructor(private todoService:TodoDataService,private route:ActivatedRoute) { }
+  constructor(private todoService:TodoDataService,private route:ActivatedRoute,private router:Router) { }
 
   ngOnInit() {
     this.id=this.route.snapshot.params['id']
@@ -24,7 +24,16 @@ export class TodoComponent implements OnInit {
           this.todo=data
         }
       )
-    
+     
   }
 
+  saveTodo(){
+    this.todoService.updateTodo("chamith",this.id,this.todo)
+        .subscribe ( 
+          data =>{
+            console.log(data);
+            this.router.navigate(['todos'])
+          }
+        )
+  }
 }
